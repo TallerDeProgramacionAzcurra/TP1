b84b87a6c0ff5a9a142aecb51a35a03cd85f58d2
Gaston Montes <gmontes@technisys.com>
2016-04-13 05:22:32 -0300

    Simple Server - Client model.

--- Server.cpp
+++ Server.cpp
@@ -6,8 +6,10 @@
 //  Copyright © 2016 Gaston Montes. All rights reserved.
 //
 
-#include <stdio.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
 #include <iostream>
+#include <stdio.h>
 
 #include "ServerSocket.hpp"
 #include "ServerThread.hpp"
@@ -16,19 +18,27 @@ static int const kSocketPort = 43210;
 static int const kSocketBacklog = 10;
 
 int main(int argc, const char * argv[]) {
-    ServerSocket serverSocket(kSocketPort, kSocketBacklog);
-    serverSocket.serverSocketBind();
-    serverSocket.serverSocketListenConnections();
+    int serverSocketFD = socket(AF_INET, SOCK_STREAM, 0);
+    int yes = 1;
+    setsockopt(serverSocketFD, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int));
     
-    int clientSocketFD = serverSocket.serverSocketAcceptConnection();
-    Socket clientSocket(clientSocketFD);
+    struct sockaddr_in serverAddress;
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = htons(kSocketPort);
+    serverAddress.sin_addr.s_addr = INADDR_ANY;
+    memset(&(serverAddress.sin_zero), '\0', 8);
+    
+    bind(serverSocketFD, (struct sockaddr *)&serverAddress, sizeof(struct sockaddr));
+    
+    listen(serverSocketFD, kSocketBacklog);
+    
+    struct sockaddr_in clientAddress;
+    socklen_t addressInSize = sizeof(struct sockaddr_in);
+    int clientSocketFD = accept(serverSocketFD, (struct sockaddr *)&clientAddress, &addressInSize);
     
     std::string dataToSend = "Socket server envía datos al cliente.";
-    clientSocket.socketSend(dataToSend);
-    
-    serverSocket.socketShutdown();
-    clientSocket.socketShutdown();
-    
+    size_t result = send(clientSocketFD, dataToSend.c_str(), dataToSend.size(), 0);
+    printf("El server envío %lu/%lu datos al cliente del texto: %s\n", result, dataToSend.size(), dataToSend.c_str());
 //
 //    ServerThread serverThread(serverSocket);
 //    
