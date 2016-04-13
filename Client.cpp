8396a159dca80cc790a3ef20d0668972531a1b21
Gaston Montes <gmontes@technisys.com>
2016-04-13 04:21:32 -0300

    Client - Simple get data.

--- Client.cpp
+++ Client.cpp
@@ -6,6 +6,8 @@
 //  Copyright © 2016 Gaston Montes. All rights reserved.
 //
 
+#include <netinet/in.h>
+#include <arpa/inet.h>
 #include <sys/socket.h>
 #include <iostream>
 #include <string.h>
@@ -18,20 +20,37 @@
 static std::string const kSocketAddress = "127.0.0.1";
 static int const kSocketPort = 43210;
 
+struct sockaddr_in getServerAddress() {
+    struct sockaddr_in newAddr;
+    newAddr.sin_family = AF_INET;
+    newAddr.sin_port = htons(kSocketPort);
+    inet_pton(AF_INET, kSocketAddress.c_str(), &newAddr.sin_addr);
+    memset(&(newAddr.sin_zero), 0, sizeof(newAddr.sin_zero));
+    return newAddr;
+}
+
 int main(int argc, const char * argv[]) {
-    ClientSocket clientSocket(kSocketAddress, kSocketPort);
-    clientSocket.clientSocketConnect();
+//    ClientSocket clientSocket(kSocketAddress, kSocketPort);
+//    clientSocket.clientSocketConnect();
+    
+    int socketFD = socket(AF_INET, SOCK_STREAM, 0);
+    
+    struct sockaddr_in serverAddress = getServerAddress();
+    struct sockaddr *address = (struct sockaddr *)&serverAddress;
+    socklen_t addressSize = sizeof(struct sockaddr);
+    connect(socketFD, address, addressSize);
+    
     
     std::string dataToRecieve = "Socket server envía datos al cliente.";
     std::vector<char> buffer;
     buffer.resize(dataToRecieve.size());
-    size_t result = recv(clientSocket.socketGetFD(), &(buffer[0]), dataToRecieve.size(), 0);
+    size_t result = recv(socketFD, &(buffer[0]), dataToRecieve.size(), 0);
     std::string bufferStr(buffer.begin(), buffer.end());
-    
+//
 //    clientSocket.socketRecieve(dataToSend.size());
 //    clientSocket.socketShutdown();
     
-    printf("Client.cpp - Datos rcibidos: %lu/%lu cuyo texto: %s\n", result, dataToRecieve.size(), bufferStr.c_str());
+    printf("Client.cpp - Datos recibidos: %lu/%lu cuyo texto: %s\n", result, dataToRecieve.size(), bufferStr.c_str());
     
 //    ClientThread clientThread;
 //    
