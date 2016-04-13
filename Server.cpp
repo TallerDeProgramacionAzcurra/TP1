b24923101c10e33733e71510352080ce749fd64a
Gaston Montes <gmontes@technisys.com>
2016-04-13 06:09:43 -0300

    Server - Accept client class.

--- Server.cpp
+++ Server.cpp
@@ -54,7 +54,12 @@ int main(int argc, const char * argv[]) {
     std::string dataToSend = "Socket server envía datos al cliente.";
     size_t result = send(clientSocketFD, dataToSend.c_str(), dataToSend.size(), 0);
     printf("El server envío %lu/%lu datos al cliente del texto: %s\n", result, dataToSend.size(), dataToSend.c_str());
+    
+    Socket *clientSocket = new Socket(clientSocketFD);
+    clientSocket->socketSend(dataToSend);
 
+    close(clientSocketFD);
+    delete clientSocket;
     serverSocket.socketShutdown();
     
 //    ServerThread serverThread(serverSocket);
