ef6dd3c8e750cebb75b004c0324454c4217c814f
Gaston Montes <gmontes@technisys.com>
2016-04-15 01:51:42 -0300

    Client - Socket message with error.

--- ClientSocket.cpp
+++ ClientSocket.cpp
@@ -40,7 +40,7 @@ void ClientSocket::clientSocketConnect() {
     
     int result = connect(this->socketFD, address, addressSize);
     if (result == kSocketError) {
-        printf("ClientSocket.cpp - Socket Connect error: %sn\n", strerror(errno));
+        printf("ClientSocket.cpp - Socket Connect error: %s\n", strerror(errno));
         this->socketClose();
         exit(1);
     }
